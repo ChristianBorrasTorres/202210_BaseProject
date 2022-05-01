6a3c9682d0728aa05ffea2adebc722cba13e2466
ChristianBorrasTorres <c.borras@uniandes.edu.co>
2022-05-01 16:25:01 -0500

    Creado el servicio que hace uso de la URL con el conjunto de datos de plantas

--- src/app/plantas/plantas.component.ts
+++ src/app/plantas/plantas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Planta } from './planta';
+import { PlantaService } from './planta.service';
 
 @Component({
   selector: 'app-plantas',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plantas.component.css']
 })
 export class PlantasComponent implements OnInit {
+  plantas: Array<Planta> = [];
+  constructor(private plantaService: PlantaService) { }
 
-  constructor() { }
+  getPlantas() {
+    this.plantaService.getPlantas().subscribe(plantas => {
+      this.plantas = plantas;
+    });
+  }
 
   ngOnInit() {
+    this.getPlantas();
   }
 
 }
